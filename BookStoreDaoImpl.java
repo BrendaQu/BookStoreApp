b1f7fc42ec34866c53a2688c440f14f47c634231
BrendaQu <brenda.quach@colorado.edu>
2021-11-04 17:00:04 -0600

    half of methods are implemented

--- BookStoreDaoImpl.java
+++ BookStoreDaoImpl.java
@@ -1,4 +1,85 @@
 package com.company;
 
-public class BookStoreDaoImpl {
+import java.sql.*;
+import java.sql.Connection;
+
+public class BookStoreDaoImpl implements BookStoreDao{
+    Connection connection;
+    public BookStoreDaoImpl(){this.connection = ConnectionFactory.getConnection(); }
+
+    @Override
+    public User getUserLogin(String email, String password) throws SQLException {
+        User user = new User();
+        String sql = "select * from users where username = '" + email + "' and password = '" + password + "'";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.next();
+        if(resultSet != null){
+            int id = resultSet.getInt(1);
+            String username = resultSet.getString(2);
+            String pass = resultSet.getString(3);
+            user = new User(id, username, pass);
+        } else
+            System.out.println("No user record found \n");
+        return user;
+    }
+
+    @Override
+    public void registerUser(String email, String password) throws SQLException {
+        String sql = "insert into users (username, password) values ( ? , ? )";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, password);
+
+        int count = preparedStatement.executeUpdate();
+        if(count > 0)
+            System.out.println("New user account created. \n");
+        else
+            System.out.println("Account was not created. Something went wrong. \n");
+    }
+
+    @Override
+    public void showCategories() throws SQLException {
+        String sql = "Select * from categories";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()){
+            System.out.println("Category ID: " + resultSet.getInt(1) + " Category Name: " + resultSet.getString(2));
+        }
+    }
+
+    @Override
+    public void showBooksByCatId(int catId) throws SQLException {
+        String sql = "select * from books where catId = '" + catId +"'";;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()){
+            System.out.println("Book ID: " + resultSet.getInt(1) + "Title: " + resultSet.getString(4));
+        }
+    }
+
+    @Override
+    public void showBookDetails(int bookId) throws SQLException {
+        String sql = "select bookId, title, author, ISBN from books where catId = '" + bookId + "'";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            System.out.println("Book ID: " + resultSet.getInt(1) + " Title: " + resultSet.getString(4) + " Author: " + resultSet.getString(5) + " ISBN: " + resultSet.getString(3));
+        }
+    }
+
+    @Override
+    public void addShopCart(int bookId, int userId) {
+
+    }
+
+    @Override
+    public void viewCart(int userId) {
+
+    }
+
+    @Override
+    public void cancelBook(int userId, int bookId) {
+
+    }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,76 @@
 package com.company;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 	// write your code here
+        BookStoreDao dao = BookStoreDaoFactory.getBookStoreDao();
+        Scanner scanner = new Scanner(System.in);
+        boolean login_menu = true;
+        while(login_menu) {
+            //Login Menu
+            System.out.println("Welcome to our bookstore!");
+            System.out.println("Press 1: User Login");
+            System.out.println("Press 2: Register Account");
+            System.out.println("Press 2: Exit bookstore \n");
+
+            int loginType = scanner.nextInt();
+            scanner.nextLine();
+            switch (loginType) {
+                case 1:
+                    boolean user_portal = true;
+                    System.out.println("Please enter your login information: ");
+                    System.out.println("Username: ");
+                    String username = scanner.nextLine();
+                    System.out.println("Password: ");
+                    String password = scanner.nextLine();
+                    User user = dao.getUserLogin(username, password);
+                    System.out.println("Welcome " + user.getUsername() + "! \n");
+                    while(user_portal) {
+                        //User Main Menu
+                        System.out.println("\n User Menu: ");
+                        System.out.println("Press 1: View all book categories");
+                        System.out.println("Press 2: View Cart");
+                        System.out.println("Press 3: Logout");
+                        int main_menu = scanner.nextInt();
+                        scanner.nextLine();
+                        switch (main_menu) {
+                            case 1:
+                                dao.showCategories();
+                                //Book Menu
+                                System.out.println("\n Press a category ID to view books in that genre: ");
+                                int cat_ID = scanner.nextInt();
+                                dao.showBooksByCatId(cat_ID);
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                user_portal = false;
+                                System.out.println("Logging out");
+                            default:
+                                System.out.println("Select 1-3.");
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Register Below: ");
+                    System.out.println("Username: ");
+                    String n_username = scanner.nextLine();
+                    System.out.println("Password: ");
+                    String n_password = scanner.nextLine();
+                    dao.registerUser(n_username, n_password);
+                    break;
+                case 3:
+                    login_menu = false;
+                    System.out.println("Good bye!");
+                    break;
+                default:
+                    System.out.println("Please select 1 or 2.");
+            }
+        }
+
     }
 }
